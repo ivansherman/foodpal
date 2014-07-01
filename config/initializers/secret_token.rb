9b7f472f5a036bd82a1a1a74395bb6f163aa0692
Ivan Sherman <ivansherman@me.com>
2014-07-01 13:10:15 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foodpal::Application.config.secret_key_base = '7075ba0e98e02a9d08f734d37477a0d960f937c17970fb12af9e532f851e991ddb86552f1a267663b2c176ddd749b481c5371f8e929a640df41904d446c2f383'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Foodpal::Application.config.session_store :cookie_store, key: '_foodpal_session'
